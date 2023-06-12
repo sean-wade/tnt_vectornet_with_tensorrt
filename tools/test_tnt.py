df65d162b59547e87deb501b12009710c0688bf3
zhanghao2 <zhanghao2@sg.cambricon.com>
2023-06-12 09:57:26 +0800

    modified test_tnt.py

--- tools/test_tnt.py
+++ tools/test_tnt.py
@@ -1,6 +1,6 @@
 '''
 Author: zhanghao
-LastEditTime: 2023-04-28 10:05:02
+LastEditTime: 2023-06-12 09:57:19
 FilePath: /my_vectornet_github/tools/test_tnt.py
 LastEditors: zhanghao
 Description: 
@@ -42,10 +42,13 @@ def test(args):
     
     # data loading
     try:
-        test_set = SGTrajDataset(test_path_list, in_mem=args.on_memory)
+        test_set = SGTrajDataset(test_path_list, in_mem=args.on_memory, num_features=args.num_features)
     except:
         raise Exception("Failed to load the data, please check the dataset!")
-
+    
+    # print("len dataset = ", len(test_set))
+    # print("dataset.num_features = ", test_set.num_features)
+    
     # init trainer
     trainer = TNTTrainer(
         trainset=test_set,
@@ -54,14 +57,16 @@ def test(args):
         collate_fn=collate_list,
         batch_size=args.batch_size,
         num_workers=args.num_workers,
-        aux_loss=True,
+        num_global_graph_layer=args.num_glayer,
+        aux_loss=False,
         with_cuda=args.with_cuda,
         cuda_device=args.cuda_device,
         save_folder=args.save_dir,
         ckpt_path=args.resume_checkpoint if hasattr(args, "resume_checkpoint") and args.resume_checkpoint else None,
         model_path=args.resume_model if hasattr(args, "resume_model") and args.resume_model else None
     )
-
+    
+    print("TNTTrainer init...")
     trainer.test(miss_threshold=2.0, 
                 save_pred=args.save_pred, 
                 convert_coordinate=True,
@@ -73,13 +78,17 @@ def test(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("-d", "--data_root", type=str, default="/mnt/data/SGTrain/rosbag/bag3/Test/feats_heading_diamond_dim10/", help="root dir for datasets")
+    parser.add_argument("-d", "--data_root", type=str, default="/home/jovyan/zhdata/TRAJ/EXP8_Heading_Diamond_DIM10_BALANCE/", help="root dir for datasets")
     parser.add_argument("-s", "--split", type=str, default="val")
 
     parser.add_argument("-b", "--batch_size", type=int, default=64, help="number of batch_size")
-    parser.add_argument("-w", "--num_workers", type=int, default=4, help="dataloader worker size")
+    parser.add_argument("-w", "--num_workers", type=int, default=0, help="dataloader worker size")
     parser.add_argument("-c", "--with_cuda", action="store_true", default=True, help="training with CUDA: true, or false")
     parser.add_argument("-cd", "--cuda_device", type=int, default=0, help="CUDA device ids")
+    
+    parser.add_argument("-nf", "--num_features", type=int, default=10)       
+    parser.add_argument("-l", "--num_glayer", type=int, default=1,
+                        help="number of global graph layers")
 
     parser.add_argument("-rc", "--resume_checkpoint", type=str,
                         # default="checkpoint_iter26.ckpt",
@@ -91,6 +100,6 @@ if __name__ == "__main__":
     parser.add_argument("-sd", "--save_dir", type=str, default="work_dir/test/"),
     parser.add_argument("-sv", "--save_pred", action="store_true", default=True)
     parser.add_argument("-npl", "--noplot", action="store_true", default=False)
-    parser.add_argument("--on_memory", type=bool, default=False, help="Loading on memory: true or false")
+    parser.add_argument("--on_memory", type=bool, default=True, help="Loading on memory: true or false")
     args = parser.parse_args()
     test(args)
